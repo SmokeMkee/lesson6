ac157958e9babefcff59db48c5674026c17e003b
SmokeMkee <83024391+SmokeMkee@users.noreply.github.com>
2022-06-16 15:30:08 +0600

    bug fix

---
+++ lib/generated/intl/messages_ru_RU.dart
@@ -0,0 +1,61 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ru_RU locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ru_RU';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "auth": MessageLookupByLibrary.simpleMessage("Авторизация"),
+        "close": MessageLookupByLibrary.simpleMessage("Закрыть"),
+        "countPersons":
+            MessageLookupByLibrary.simpleMessage("Всего персонажей: "),
+        "counterValue":
+            MessageLookupByLibrary.simpleMessage("Значение счетчика"),
+        "english": MessageLookupByLibrary.simpleMessage("Английский"),
+        "genderGirl": MessageLookupByLibrary.simpleMessage("Человек, Женский"),
+        "genderMan": MessageLookupByLibrary.simpleMessage("Человек, Мужской"),
+        "homePage": MessageLookupByLibrary.simpleMessage("Домашняя страница"),
+        "inputErrorCheckLogin":
+            MessageLookupByLibrary.simpleMessage("Проверьте логин"),
+        "inputErrorCheckPassword":
+            MessageLookupByLibrary.simpleMessage("Проверьте пароль"),
+        "inputErrorLoginIsLong": MessageLookupByLibrary.simpleMessage(
+            "Логин должен содержать не более 8 символов"),
+        "inputErrorLoginIsShort": MessageLookupByLibrary.simpleMessage(
+            "Логин должен содержать не менее 3 символов"),
+        "inputErrorPasswordIsLong": MessageLookupByLibrary.simpleMessage(
+            "Пароль должен содержать не более 16 символов"),
+        "inputErrorPasswordIsShort": MessageLookupByLibrary.simpleMessage(
+            "Пароль должен содержать не менее 8 символов"),
+        "inputLoginAndPassword":
+            MessageLookupByLibrary.simpleMessage("Введите логин и пароль"),
+        "language": MessageLookupByLibrary.simpleMessage("Язык"),
+        "login": MessageLookupByLibrary.simpleMessage("Логин"),
+        "password": MessageLookupByLibrary.simpleMessage("Пароль"),
+        "persons": MessageLookupByLibrary.simpleMessage("Персонажи"),
+        "russian": MessageLookupByLibrary.simpleMessage("Русский"),
+        "searchPerson": MessageLookupByLibrary.simpleMessage("Найти персонажа"),
+        "settings": MessageLookupByLibrary.simpleMessage("Настройки"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Вход"),
+        "statusAlive": MessageLookupByLibrary.simpleMessage("Живой"),
+        "statusDead": MessageLookupByLibrary.simpleMessage("Мертв"),
+        "tryAgain": MessageLookupByLibrary.simpleMessage("Попробуйте снова")
+      };
+}
